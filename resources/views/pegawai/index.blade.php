2520fdc04a6a341ef0e6cbc662987d73bd902aa0
Adi Darmawan <adidrmwan17@gmail.com>
2019-09-23 11:50:55 +0700

    tampilan rekomen

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -23,6 +23,23 @@
                     </div>
                 </div>
             </div>
+            <div class='col-md-6'>
+                <div class="layers bd bgc-white p-20">
+                    <div class="layer w-100 mB-10">
+                        <h6 class="lh-1">Penilaian dari Kompetensi Pegawai :</h6>
+                    </div>
+                    <div class="layer w-100">
+                        <div class="peers ai-sb fxw-nw">
+                            <div class="peer peer-greed">
+                                <span id="sparklinedash"></span>
+                            </div>
+                            <div class="peer">
+                                <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-green-50 c-green-500">Anda Berhak Naik jabatan</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="masonry-item col-md-6" style="margin:0 auto; float:none; position:relative; display: block;">
